78d4a5c75b7e7a41672e619fca65f451c029aebe
CoutoDev <c.svitor@yahoo.com.br>
2021-02-04 00:01:35 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
-
+import { Exercise } from "./exercise";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-birl';
+  exercises: Exercise[] = [{
+    name: 'Abdominal',
+    duration: 30,
+    repetition: 3,
+    preparation:15, 
+    rest: 20
+  }];
+  config: boolean = false;
 }
